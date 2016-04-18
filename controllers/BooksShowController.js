3ec7fb3a07ff028d854fa01f4a9d15df2ca08761
Nathan L <adamnate@gmail.com>
2016-04-18 13:22:35 -0700

    last commit

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -4,4 +4,36 @@ angular.module('libraryApp')
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
+
+  
+  $http({
+    method: "GET",
+    url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id
+  }).then(function(response){
+    vm.books = response.data;
+  })
+
+vm.editBook = function(){
+  $http({
+    method:"PUT",
+    url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
+    data: {
+      title: vm.book.title,
+      author: vm.book.author,
+      image: vm.book.image,
+      releaseDate: vm.book.releaseDate
+    }
+  }).then(function(response){
+    vm.book = response.data;
+    $location.path('/');
+  })
+}
+vm.deleteBook = function(){
+  $http({
+    method:"DELETE",
+    url: 'https://super-crud.herokuapp.com/books/' + $routeParams.id,
+  }).then(function(response){
+    vm.book = response.data;
+    $location.path('/');
+}
 };
