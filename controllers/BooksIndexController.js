b8e7d8055f5dba1227dd4ab32cf79663d88cfa3c
Nathan L <adamnate@gmail.com>
2016-04-18 11:31:44 -0700

    adding al beginning info on index

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -9,6 +9,7 @@ $http({
   url: 'https://super-crud.herokuapp.com/books'
 }).then(function(response){
   console.log(response.data);
+  vm.books = response.data.books;
 }).then(function(error){
   console.log(error);
 });
